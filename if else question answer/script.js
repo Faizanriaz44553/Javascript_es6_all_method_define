10b6f7dd51d07d75f853be62b28fe29b194956d2
Faizanriaz44553 <sylanipractice@gmail.com>
2026-07-20 16:35:16 -0700

    project initialization

---
+++ if else question answer/script.js
@@ -0,0 +1,175 @@
+// Q1: Check if a number is positive or negative.
+
+// var number = prompt('enter your number')
+// if (Number(number)> 0) {
+//     alert(`yes ${number} is positive number`)
+//     console.log('ok');
+// } else {
+//     alert(`no this number is nagitive number`)
+//     console.log("no");
+// }
+
+// _______________________________________________________________________________________________________
+
+// Q2: Check if a number is even or odd.
+
+// var num = Number(prompt("enter your number"))
+// if (num % 2 === 0) {
+//     alert('even')
+// } else {
+//     alert('odd')
+// }
+
+// _____________________________________________________________________________________________
+
+//Q3: Check if a person's age is 18 or above. If yes, print "Eligible to vote", otherwise print "Not eligible to vote".
+
+// var num = Number(prompt('enter your age'))
+// if (num >= 18) {
+//     alert('Eligible to vote')
+// } else {
+//     alert('Not eligible to vote')
+// }
+
+// _________________________________________________________________________________________________
+
+//Q4: Check if a student has passed. Passing marks are 50.
+
+// var num = Number(prompt('enter your mark'));
+
+// if (num >= 50) {
+//     alert('congratulation! you are passed')
+// } else {
+//     alert('sorry you are failed')
+// }
+
+// _____________________________________________________________________________________________________
+
+//Q5: Check if a number is greater than 100. If yes, print "Large Number", otherwise print "Small Number".
+
+// var num = Number(prompt('enter your number'));
+// if (num > 100) {
+//     alert('Large Number')
+// } else {
+//     alert('Small Number')
+// }
+
+// __________________________________________________________________________________________________________
+
+//Q6: Check if a password is equal to "admin123". If yes, print "Login Successful", otherwise print "Incorrect Password".
+
+// var pass = prompt('enter your password');
+// var correctPass = 'admin123'
+// if (pass === correctPass) {
+//     alert('Login Successful')
+// } else {
+//     alert('Incorrect Password')
+// }
+
+// ___________________________________________________________________________________________________________
+
+//Q7: Check if a user has enough balance to buy an item. If the balance is greater than or equal to the item price, print "Purchase Successful", otherwise print "Insufficient Balance".
+
+// var balance = Number(prompt('This mouse price is 1000'))
+// var price = 1000
+// if (balance >= price) {
+//     alert(`
+//         Purchased Successful 
+//         Balanced: ${balance}
+//         Mouse price: ${price}
+//         calculate:  ${balance} - ${price}
+//         Current balanced: ${balance - price}`)
+// } else {
+//     alert('Insufficient Balance')
+// }
+
+// _______________________________________________________________________________________________
+
+//Q8: Check if a temperature is above 30°C. If yes, print "It's Hot", otherwise print "Weather is Pleasant".
+
+// var num = Number(prompt('Enter your tempreture'))
+// if (num > 30 ) {
+//     alert("It's Hot")
+// } else {
+//     alert("Weather is Pleasant")
+// }
+
+// ____________________________________________________________________________________________________
+
+//Q9: Check if a number is divisible by 5. 
+
+// var number = Number(prompt('enter your number'));
+// if (number% 5 ===0) {
+//     alert('yes divisible')
+// } else {
+//     alert('no divisible')
+// }
+
+// ____________________________________________________________________________________________________
+
+//Q10: Check if a traffic light is "green". If yes, print "Go", otherwise print "Stop".
+
+// var txt = prompt('enter traffic light')
+// var green = "green"
+// if (txt === green) {
+//     alert('Go')
+// } else {
+//     alert('Stop')
+// }
+
+// ________________________________________________________________________________________________________
+
+//Q11: Check if a username is "admin". If yes, print "Welcome Admin", otherwise print "Access Denied".
+
+// var userName = prompt('enter username');
+// if (userName === "admin") {
+//     alert("Welcome Admin")
+// } else {
+//     alert("Access Denied")
+// }
+
+// _________________________________________________________________________________________________________
+
+//Q12: Check if a person's height is 170 cm or more. If yes, print "Tall", otherwise print "Average Height".
+
+// var height = Number(prompt('Enter your height in cm'));
+// if (height >= 170) {
+//     alert('Tall')
+// } else {
+//     alert('Average Height')
+// }
+
+// _________________________________________________________________________________________________________
+
+//Q13: Check if a user is logged in (true or false). If logged in, print "Welcome Back", otherwise print "Please Login".
+
+// var isloggedin = true;
+// if (isloggedin) {
+//     console.log("Welcome Back");
+// } else {
+//     console.log("Please Login");
+// }
+
+// ____________________________________________________________________________________________________________
+
+//Q14: Check if a person's age is 60 or above. If yes, print "Senior Citizen", otherwise print "Not a Senior Citizen".
+
+// var person = Number(prompt("Enter your age"));
+// if (person >= 60) {
+//     alert("Senior Citizen")
+// } else {
+//     alert("Not a Senior Citizen")
+// }
+
+// ________________________________________________________________________________________________________
+
+//Q15: Check if a shop is open (true or false). If open, print "Shop is Open", otherwise print "Shop is Closed"
+
+// var isOpen = true
+// if (isOpen) {
+//     console.log("Shop is Open");
+// } else {
+//     console.log("Shop is Closed");
+// }
+
+// ___________________________________________________________________________________________________________
