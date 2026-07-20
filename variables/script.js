45e0addfbf671083b00cbdfd12c87389d2632947
Faizanriaz44553 <sylanipractice@gmail.com>
2026-07-19 17:18:50 -0700

    project initialization

---
+++ variables/script.js
@@ -0,0 +1,104 @@
+// Question 1: Personal Information
+
+// var name = prompt('enter your name');
+// var age = prompt('enter your age');
+// var city = prompt('enter your city name ')
+
+
+// alert('Hello' + ' ' + name + `\n` + 'You are' + ' ' + age + ' ' + 'years old.' + `\n` + 'You live in' + ' ' + city);
+// _______________________________________________________________________________________________
+
+// Question 2: Sum of Two Numbers
+
+// var firstNum = prompt('Enter your first digit');
+// var sum = Number(firstNum) + Number(secondNum);
+// alert(`The sum of two numbers is: ${sum}`);
+// ________________________________________________________________________________________________
+
+// Question 3: Basic Calculator
+
+// var firstNum = prompt("enter first number");
+// var secondNum= prompt("enter second number");
+// var number1 = Number(firstNum);
+// var number2 = Number(secondNum);
+
+//     alert(` Addition: ${number1 + number2} \n Subtraction: ${number1 - number2} \n Multiplication: ${number1 * number2} \n Division: ${number1 / number2}`)
+
+
+// _________________________________________________________________________________________________________
+
+// Question 4: Rectangle Area
+
+// var widht = prompt('enter your area widht');
+// var length = prompt('enter your area length');
+// var number1 = Number(widht);
+// var number2 = Number(length);
+
+//     alert(`${number1 * number2} Square cm(cm2)`)
+
+
+// _________________________________________________________________________________________________________
+
+// Question 5: Circle Circumference
+
+// var number = prompt('enter your radius');
+// var radius = Number(number);
+
+//     var Circumference = 2* Math.PI * radius
+//     alert(`your Circumference is ${Circumference}`)
+
+// ________________________________________________________________________________________________________
+
+// Question 6: Student Marks
+
+// var name = prompt('Enter your name');
+// var English = prompt('Enter your english mark');
+// var Math = prompt('Enter your math mark');
+// var Science = prompt('Eter your science mark');
+
+// var result = Number(English) + Number(Math) + Number(Science)
+// alert(`
+//        Name: ${name}
+//        English: ${English}
+//        Math: ${Math}
+//        Science: ${Science}
+//        Obtained Marks: ${result} / 300`
+//     )
+
+// ______________________________________________________________________________________________
+
+// Question 7: Age After 10 Years
+
+// var age = prompt('enter your age');
+// alert(`after 10 years your age is ${Number(age) + 10}`)
+
+// ___________________________________________________________________________________________________
+
+// Question 8: Currency Conversion
+
+// var usd = prompt('Enter your amount in USD');
+// alert('converted your currency USD:' + " " + usd + " " + "in PKR:" + " " + Number(usd)*285)
+
+
+// _______________________________________________________________________________________________________
+
+// Question 9: Favorite Programming Language
+
+// var name = prompt('Enter your name');
+// var language = prompt("Enter your favorite programming language");
+
+// alert('Hello'+ ' ' + name + " " + 'Your favorite programming language is '+  ' ' + language + '.' + ' ' + 'Keep practicing!')
+
+// _________________________________________________________________________________________________________
+
+// Question 10: Simple Receipt
+
+// var pname = prompt('Product name');
+// var quentity = prompt('Quentity?');
+// var price = prompt('Price?');
+
+// alert(`
+//     Product: ${pname}
+//     Price: ${price}
+//     Quantity: ${quentity}
+//     Total: ${Number(quentity) * Number(price)}`)
